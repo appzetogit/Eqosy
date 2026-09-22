2945a88fd6c00b892121f09ac03647ce474f861f
upma-dev <upma7354@gmail.com>
2026-09-22 12:03:07 +0530

    changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -49,7 +49,6 @@ export default defineConfig({
         ]
       },
       workbox: {
-        suppressWarnings: true,
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         cleanupOutdatedCaches: true,
         skipWaiting: true,
